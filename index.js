91876ff8fb5afe5d8c32ae18029b6282ad0ee0cc
yurism <mascarenhasyuri@hotmail.com>
2024-04-02 01:43:53 -0300

    [Add] views page independent from the cwd

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import * as path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
