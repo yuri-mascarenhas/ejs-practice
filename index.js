df3aa113d934b6483867ab95a900076178d856c2
yurism <mascarenhasyuri@hotmail.com>
2024-04-02 01:22:23 -0300

    [Add] Navigation functionality

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Root path");
+  res.render("home.ejs");
+});
+
+app.get("/info", (req, res) => {
+  res.render("info.ejs");
 });
 
 app.listen(3000, () => {
