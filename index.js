60a198dce4966992b69f1064f38ce184ab363b4c
yurism <mascarenhasyuri@hotmail.com>
2024-04-17 08:29:57 -0300

    [Add] Express static assets folder

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+// Routes
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
